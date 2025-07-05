83de22365caa3c85603ed7cc646abb2b1bf5f927
Rehan malik <malikr96204@gmail.com>
2025-07-05 14:08:34 +0530

    added hii

--- Rehan.java
+++ Rehan.java
@@ -3,6 +3,7 @@ package LocalRepo;
 public class Rehan {
     public static void main(String[] args) {
         System.out.println("rehannnnn");
+        System.out.println("hiiiiiiiiiii");
         
     }
     
